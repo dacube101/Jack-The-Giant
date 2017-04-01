c000a718dbaed71cb55e82f2b18c62b9b6410afe
dacube101 <rafael10123564@outlook.com>
2017-04-01 18:43:01 -0400

    backround debug

---
+++ GameplayScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameplayScene.swift
+//  Jack The Giant
+//
+//  Created by rafael rivera on 4/1/17.
+//  Copyright © 2017 rafael rivera. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene {
+    override func didMove(to view: SKView) {
+        print ("the scene was loaded");
+    }
+}
